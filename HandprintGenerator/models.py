6fa7a17b9aade05b83d9fd97ca73abf127d22c52
Jennifer Jin <jj1@andrew.cmu.edu>
2016-03-15 00:05:30 -0400

    role and date created fields added

--- HandprintGenerator/models.py
+++ HandprintGenerator/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+ROLE_CHOICES = (
+    ('admin', 'Administrator'), 
+    ('mod', 'Moderator'), 
+    ('member', 'Member')
+    )
+
+
 class User(models.Model):
     username = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
@@ -7,7 +14,12 @@ class User(models.Model):
     password = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
     location = models.CharField(max_length=50, null=True, blank=True)
-#    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    role = models.CharField(
+        max_length = 15, 
+        blank = False, 
+        choices = ROLE_CHOICES,
+        default = 'member')
 
 class ActionItem(models.Model):
     creator = models.ForeignKey(User)
@@ -16,13 +28,13 @@ class ActionItem(models.Model):
     references = models.CharField(max_length=500)
     images = models.CharField(max_length=500)
     active = models.BooleanField(default=True)
- #   date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
 
 class ActionItemComment(models.Model):
     action_item = models.ForeignKey(ActionItem)
     name = models.CharField(max_length=50)
     text = models.CharField(max_length=1000)
-#    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
 
 class ActionItemTag(models.Model):
     action_item = models.ForeignKey(ActionItem)
@@ -35,6 +47,6 @@ class ActionItemVote(models.Model):
 class ActionItemInactive(models.Model):
     action_item = models.ForeignKey(ActionItem)
     reason = models.CharField(max_length=150)
-#    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
 
 #reason - should be drop down -> duplicate, other, 
