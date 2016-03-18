d3353537f96d828a47177e8fbb6445ed9c165b82
Mark Vella <mvella@andrew.cmu.edu>
2016-03-18 13:20:46 -0400

    Cleaned up all HTML with doctype, header, body tags. Added nav bar to every page with links. Added index to point to action idea index.

--- HandprintGenerator/urls.py
+++ HandprintGenerator/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'HandprintGenerator'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index', views.index, name='index'),
 	url(r'^(?P<actionitem_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^create_user', views.new_user, name='new_user'),
 	url(r'^new', views.new_action_item, name='new_action_item'),
