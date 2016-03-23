138ce50cfaddc14396808a4c50ddf3bef7f3ce56
Javed Ramjohn <jramjohn@andrew.cmu.edu>
2016-03-22 21:21:51 -0400

    Static Pages and URL routes added/tested.

--- HandprintGenerator/urls.py
+++ HandprintGenerator/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from . import views
+from django.views.generic import TemplateView
 
 app_name = 'HandprintGenerator'
 urlpatterns = [
@@ -10,5 +11,9 @@ urlpatterns = [
     url(r'^create_user', views.new_user, name='new_user'),
     url(r'^new', views.new_action_item, name='new_action_item'),
     url(r'^users', views.user_index, name='user_index'),
+    url(r'^about', TemplateView.as_view(template_name='about_us.html'), name="about_us"),
+    url(r'^contact', TemplateView.as_view(template_name='contact_us.html'), name="contact_us"),
+    url(r'^privacy', TemplateView.as_view(template_name='privacy_policy.html'), name="privacy_policy"),
+    url(r'^terms', TemplateView.as_view(template_name='terms_of_service.html'), name="terms_of_service"),
 
 ]
