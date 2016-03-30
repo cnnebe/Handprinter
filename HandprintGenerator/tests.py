06d9c8d07cb062d54e542cd60373820cf493e362
Jennifer Jin <jj1@andrew.cmu.edu>
2016-03-30 00:36:40 -0400

    test works

--- HandprintGenerator/tests.py
+++ HandprintGenerator/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
 
 class ProfileModelTest(TestCase):
@@ -10,7 +10,7 @@ class ProfileModelTest(TestCase):
 		u3 = User.objects.create(username="mod", first_name="Joe", last_name="Smith", email="js@gmail.com")
 		p1 = Profile.objects.create(user = u1)
 		p2 = Profile.objects.create(user = u2, role = "admin")
-		p2 = Profile.objects.create(user = u3, role = "moderator")
+		p3 = Profile.objects.create(user = u3, role = "moderator")
 		self.assertEqual(p1.role, 'member')
 		self.assertEqual(p2.role, "admin")
 		self.assertEqual(p3.role, "moderator")
